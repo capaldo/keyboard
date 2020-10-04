1468fc784119bf64cba72f3581e0a0517a085c09
capaldo <alex@capaldo.io>
2020-10-04 18:28:38 -0400

    add m10c macros and keymap

--- m10c/keymap.c
+++ m10c/keymap.c
@@ -1,33 +1,76 @@
-/* Copyright 2020 Jason Williams (Wilba)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
 #define _FIRST 0
 #define _SECOND 1
 #define _THIRD 2
 
+#undef RGB_BACKLIGHT_ENABLED
+#define RGB_BACKLIGHT_ENABLED 0
+
+enum custom_keycodes {
+    YABAI_LEFT = SAFE_RANGE,
+    YABAI_RIGHT,
+    ALFRED
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case YABAI_LEFT:
+        if (record->event.pressed) {
+	    SEND_STRING(SS_DOWN(X_LCMD));
+	    SEND_STRING(SS_DOWN(X_LOPT));
+	    SEND_STRING("z");
+        } else {
+	    SEND_STRING(SS_UP(X_LCMD));
+	    SEND_STRING(SS_UP(X_LOPT));
+        }
+        break;
+    case YABAI_RIGHT:
+        if (record->event.pressed) {
+	    SEND_STRING(SS_DOWN(X_LCMD));
+	    SEND_STRING(SS_DOWN(X_LOPT));
+	    SEND_STRING("c");
+        } else {
+	    SEND_STRING(SS_UP(X_LCMD));
+	    SEND_STRING(SS_UP(X_LOPT));
+        }
+        break;
+    case ALFRED:
+        if (record->event.pressed) {
+	    SEND_STRING(SS_DOWN(X_LCMD));
+	    SEND_STRING(SS_DOWN(X_LCTL));
+	    SEND_STRING(SS_DOWN(X_LOPT));
+	    SEND_STRING(SS_DOWN(X_LSFT));
+	    SEND_STRING(" ");
+        } else {
+	    SEND_STRING(SS_UP(X_LCMD));
+	    SEND_STRING(SS_UP(X_LCTL));
+	    SEND_STRING(SS_UP(X_LOPT));
+	    SEND_STRING(SS_UP(X_LSFT));
+        }
+        break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FIRST] = LAYOUT( 
-	    KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_1, KC_KP_2, KC_KP_3, TO(_SECOND) 
+	    KC_KP_7, KC_KP_8, KC_KP_9, 
+	    YABAI_LEFT, TO(_SECOND), YABAI_RIGHT, 
+	    KC_KP_1, KC_KP_2, KC_KP_3, 
+	    ALFRED 
 	    ),
     [_SECOND] = LAYOUT( 
-	    KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_1, KC_KP_2, KC_KP_3, TO(_THIRD) 
+	    KC_KP_7, KC_KP_8, KC_KP_9, 
+	    KC_KP_4, TO(_THIRD), KC_KP_6, 
+	    KC_KP_1, KC_KP_2, KC_KP_3, 
+	    ALFRED 
 	    ),
     [_THIRD] = LAYOUT( 
-	    KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_1, KC_KP_2, KC_KP_3, TO(_FIRST) 
+	    KC_KP_7, KC_KP_8, KC_KP_9, 
+	    KC_KP_4, TO(_FIRST), KC_KP_6, 
+	    KC_KP_1, KC_KP_2, KC_KP_3, 
+	    ALFRED 
 	    )
 };
 
